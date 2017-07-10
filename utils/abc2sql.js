94830cfc204a879fd1c2d17dadd02f99856fec6c
Ralex86 <hassler@monemail.com>
2017-07-11 01:06:59 +0200

    adding utils for parsing abc, adding tuples on the database, svg convertion

---
+++ utils/abc2sql.js
@@ -0,0 +1,89 @@
+var fs = require('fs')
+var readline = require('readline')
+
+var logger = fs.createWriteStream('tunebook.sql', {
+    flags: 'a' // 'a' means appending (old data will be preserved)
+})
+
+class Abc2sql {
+    constructor(file,startId){
+        this.tunes = []
+        this.currentId = startId
+        this.file = file
+        this.lineReader = readline.createInterface({input: fs.createReadStream(file)})
+
+        this.inHeader = this.inHeader.bin(this)
+        this.run = this.run.bind(this)
+        this.readLine = this.readLine.bind(this)
+        this.addslashes = this.addslashes.bind(this)
+    }
+
+    addslashes(str){
+        return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')
+    }
+
+    inHeader(line){
+        return (
+            line[0] + line[1] != "P:"
+            && line[0].charCodeAt(0) >= "A".charCodeAt(0)
+            && line[0].charCodeAt(0) <= "Z".charCodeAt(0)
+            && line[1] === ":"
+        )
+    }
+
+    run(callback){
+        this.lineReader.on('line', this.readLine)
+        this.lineReader.on('close', () => {
+            console.log(`${this.tunes.length} tunes added !!`)
+            callback(this.tunes)
+        })
+    }
+
+    readLine(line){
+        //order matter because we have to check if line is not undefined
+        //inHeader crash if line is undef
+
+        if(!line) {return false}
+        var header = this.inHeader(line)
+        if(0 === this.currentId && !header) {return false}
+
+        if (header){
+            var field = line[0]
+            var fieldValue = line.split(":").splice(1)
+
+            if (field === "X"){
+                this.currentId = Number(fieldValue)
+                this.tunes[this.currentId -1] = {}
+                this.tunes[this.currentId -1].X = this.currentId
+                this.tunes[this.currentId -1].body = ""
+            } else {
+                if (field === "T"){
+                    this.tunes[this.currentId -1][field] = this.addslashes(fieldValue)
+                } else {
+                    this.tunes[this.currentId -1][field] = fieldValue
+                }
+            }
+        } else { 
+            this.tunes[this.currentId -1].body += this.addslashes(`${line}\n`)
+        }
+    }
+}
+
+var tunebook
+var readTunes = new Abc2sql('tunebook.abc', 0)
+
+// Lets the fun begin
+
+readTunes.run((tunebook) => {
+    for(var i = 0; i < tunebook.length; i++){
+        // T, R, M, K, body
+        logger.write(`INSERT INTO Tunes (T,R,M,K,body) VALUES(
+        "${tunebook[i].T}",
+        "${tunebook[i].R}",
+        "${tunebook[i].M}",
+        "${tunebook[i].K}",
+        "${tunebook[i].body}");\n`)
+    }
+
+    logger.end()
+})
---
+++ utils/sheet2database.js
@@ -0,0 +1,103 @@
+var mysql = require('mysql')
+var abc = require('./abc2svg.js')
+var myconf = require('./config.json')
+
+class Sheet2sql{
+    constructor(){
+        this.svg = ""
+        this.tuneID = null
+        this.src = ""
+
+        this.connection = mysql.createConnection({
+            host: myconf.host,
+            user: myconf.user,
+            password : myconf.password,
+            database: myconf.database
+        })
+
+        this.convert = this.convert.bind(this)
+        this.run = this.run.bind(this)
+        this.mysql_real_escape_string = this.mysql_real_escape_string.bind(this)
+    }
+
+    run(){
+        this.connection.connect((err) => {
+            if (err) throw err
+
+            this.connection.query('SELECT * FROM Tunes' (error, result, fields) => {
+                if (error) throw error
+
+                console.log(result)
+
+                for(var i=0; i<result.length;i++){
+                    this.tuneID = result[i].id
+                    this.src = `
+                    X: ${result[i].id}\n
+                    T: ${result[i].T}\n
+                    R: ${result[i].R}\n
+                    M: ${result[i].M}\n
+                    K: ${result[i].K}\n
+                    ${result[i].body}`
+
+                    this.convert(this.src, (svg) => {
+                        var sql = `INSERT INTO Svg (tuneID, svg) VALUES (
+                        ${result[i].id}, 
+                        "${this.mysql_real_escape_string(svg)}")`
+                        
+                        this.connection.query(sql, (err, result) => {
+                            if (err) throw err
+                            console.log("1 record inserted")
+                        })
+                    }) // convert
+                } // for loop
+            })
+        })
+    }
+    
+    mysql_real_escape_string (str) {
+        return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+            switch (char) {
+                case "\0":
+                    return "\\0" 
+                case "\x08":
+                    return "\\b" 
+                case "\x09":
+                    return "\\t" 
+                case "\x1a":
+                    return "\\z" 
+                case "\n":
+                    return "\\n" 
+                case "\r":
+                    return "\\r" 
+                case "\"":
+                case "'":
+                case "\\":
+                case "%":
+                    return "\\"+char  // prepends a backslash to backslash, percent,
+                    // and double/single quotes
+            }
+        }) 
+    }
+
+    convert(src, callback){
+        var errtxt = "", new_page = ""
+        var user = {
+            img_out: function(str){
+                this.svg = ((typeof this.svg === 'undefined') ? "" : this.svg + '\n') + str
+                if (str == "</div>")
+                    callback(this.svg)
+            },
+            errmsg: function(msg,l,c){
+                errtxt += mssg + '\n'
+                console.log(errtxt)
+                console.log('line, column', l, c)
+            },
+            page_format: true
+        }
+        var a = new abc.Abc(user)
+        a.tosvg("a", src)
+    }
+}
+
+var addTuples = new Sheet2sql
+addTuples.run()
